782d387fab5a07209fa832104520d47221a50684
AgeBK <age24@hotmail.com>
2024-04-23 07:50:35 +1000

    first dashboard commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <p>
+      Dashboard Page : One benefit of using layouts in Next.js is that on
+      navigation, only the page components update while the layout won&apos;t
+      re-render. This is called partial rendering:
+    </p>
+  );
+}
